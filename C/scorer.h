5cc73efd598b9105ce76cc5789f3ac083d1b2fda
Sean R. Lang <srlang@ncsu.edu>
2013-05-18 18:21:02 -0400

    fixed enum for Jack

--- C/scorer.h
+++ C/scorer.h
@@ -28,7 +28,7 @@
 typedef enum Cards {
     Ace = 0, Two = 1, Three = 2, Four = 3, 
     Five = 4, Six = 5, Seven = 6, Eight = 7,
-    Nine = 8, Ten = 9, Jack, Queen = 10, King = 11
+    Nine = 8, Ten = 9, Jack = 10, Queen = 11, King = 12
 } Cards_t;
 
 /* Suits of the cards. */
