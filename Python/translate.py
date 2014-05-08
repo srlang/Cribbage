3294cfc240945d93a4e65c5b0ad1caa03b648fa2
Sean R. Lang <srlang@ncsu.edu>
2014-05-08 15:20:22 -0400

    stubs for translation

---
+++ Python/translate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import re
+
+hand_re = re.compile('([0-9JQK] )+')
+
+SUITS = ['C', 'S', 'H', 'D']
+TYPES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+def hton(hand_str):
+    return
+
+def _hton(card_str):
+    return
+
+def ntoh(hand_str):
+    return 
+
+def _ntoh(card_str):
+    return
+
