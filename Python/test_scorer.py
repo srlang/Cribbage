ab2ca19e0a56cc4f626460a42dd6be7b0df5ad2a
Sean R. Lang <srlang@ncsu.edu>
2014-05-11 22:51:10 -0400

    progress made in scorer

--- Python/test_scorer.py
+++ Python/test_scorer.py
@@ -2,6 +2,8 @@
 
 import unittest
 
+from scorer import * 
+
 class TestScorer(unittest.TestCase):
 
     """
@@ -10,39 +12,58 @@ class TestScorer(unittest.TestCase):
 
     def setUp(self):
         self.stuff = []
+        self.hand1 = [1, 2, 3, 4, 5]
+        self.hand_invalid = [-1, 2, 3, 4, 5]
+        self.hand_invalid2 = [0, 1, 2, 3, 55]
 
-    def test_validHand(self):
-        self.assertTrue(True)
+    def test_valid_hand(self):
+        self.assertTrue(valid_hand(self.hand1))
+        self.assertFalse(valid_hand(self.hand_invalid))
+        self.assertFalse(valid_hand(self.hand_invalid2))
 
     def test_suit(self):
-        self.assertTrue(True)
+        self.assertEqual(0, suit(20))
+        self.assertEqual(1, suit(45))
+        self.assertEqual(2, suit(18))
+        self.assertEqual(3, suit(19))
 
     def test_value(self):
-        self.assertTrue(True)
+        self.assertEqual(10, value(40))
+        self.assertEqual(5, value(22))
+        self.assertEqual(0, value(2))
 
-    def test_numericValue(self):
+    def test_numeric_value(self):
         self.assertTrue(True)
+        self.assertEqual(6, numeric_value(22))
+        self.assertEqual(8, numeric_value(30))
+        self.assertEqual(10, numeric_value(44))
+        self.assertEqual(10, numeric_value(48))
 
     def test_pairs(self):
-        self.assertTrue(True)
+        pass
 
     def test_runs(self):
-        self.assertTrue(True)
+        pass
 
     def test_fifteens(self):
-        self.assertTrue(True)
+        pass
 
-    def test_eachType(self):
-        self.assertTrue(True)
+    def test_each_type(self):
+        # 
+        pass
 
-    def test_numberEqual(self):
-        self.assertTrue(True)
+    def test_number_equal(self):
+        arr = [1, 1, 2, 3, 5, 5, 5, 5]
+        self.assertEquals(2, number_equal(arr, 1))
+        self.assertEquals(1, number_equal(arr, 2))
+        self.assertEquals(1, number_equal(arr, 3))
+        self.assertEquals(4, number_equal(arr, 5))
 
-    def test_rightJack(self):
-        self.assertTrue(True)
+    def test_right_jack(self):
+        pass
 
     def test_score(self):
-        self.assertTrue(True)
+        pass
 
 
 if __name__ == '__main__':
